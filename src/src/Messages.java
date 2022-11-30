ace98ab0b94a6414c881c9d5ac5652b8373600cf
Logofatu Ionut <ionut20001@yahoo.com>
2022-11-30 12:38:03 +0200

    Added all messages

---
+++ src/src/Messages.java
@@ -0,0 +1,38 @@
+public class Messages {
+
+    public void messageForHighCard(int player){
+        System.out.println("Player " + player + " won with high card");
+    }
+
+    public void messageForOnePair(int player){
+        System.out.println("Player " + player + " won with one pair");
+    }
+
+    public void messageForTwoPairs(int player){
+        System.out.println("Player " + player + " won with two pairs");
+    }
+
+    public void messageForThreeOfAKind(int player){
+        System.out.println("Player " + player + " won with three of a kind");
+    }
+
+    public void messageForStraight(int player){
+        System.out.println("Player " + player + " won with straight");
+    }
+
+    public void messageForFlush(int player){
+        System.out.println("Player " + player + " won with flush");
+    }
+
+    public void messageForFullHouse(int player){
+        System.out.println("Player " + player + " won with full house");
+    }
+
+    public void messageForFourOfAKind(int player){
+        System.out.println("Player " + player + " won with four of a kind");
+    }
+
+    public void messageForStraightFlush(int player){
+        System.out.println("Player " + player + " won with straight flush");
+    }
+}
